e730d07ae7887b5d089facf29fe62fbac5b344a1
Vedez <lovelyfernandez2001@gmail.com>
2024-08-25 00:41:45 +0100

    Added username updating based on profile logged in

--- public/assets/scripts/updateui.js
+++ public/assets/scripts/updateui.js
@@ -1,25 +1,34 @@
-import { auth } from './init.js';
+import { auth, db } from './init.js';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
+import { doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js';
 
 document.addEventListener('readystatechange', () => {
     const profilePicture = document.getElementById('profile-picture');
     const signInButton = document.getElementById('sign-in-button');
-    const profileMenu = document.getElementsByClassName('profile-menu');
+    const profileMenu = document.getElementsByClassName('profile-menu')[0];
+    const usernameText = document.getElementById('user-name');
 
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async (user) => {
         if (user) {
+            const userDoc = await getDoc(doc(db, 'users', user.uid));
+
+            if (userDoc.exists() && usernameText) { 
+                usernameText.innerHTML = userDoc.data().username;
+                usernameText.style.display = 'block';
+            }
+            
 
             // User is signed in, show profile picture
-            profilePicture.style.display = 'block';
+            if (profilePicture) profilePicture.style.display = 'block';
 
             // Set picture to user picture here
 
             // Show the profile menu
-            profileMenu[0].style.display = 'block';
+            if (profileMenu) profileMenu.style.display = 'block';
 
         } else {
             // User is not signed in, show sign in
-            signInButton.style.display = 'inline-block';
+            if (signInButton) signInButton.style.display = 'inline-block';
         }
     });
 });
