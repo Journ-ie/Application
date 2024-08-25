471c7ee961c5effe1bd6809e4d79667f3822a5db
Vedez <lovelyfernandez2001@gmail.com>
2024-08-25 01:59:56 +0100

    Fixed small error with log in / create accounts

--- public/assets/scripts/init.js
+++ public/assets/scripts/init.js
@@ -17,61 +17,32 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-
-// Variable to track whether a user is signing in
-let isSigningIn = false;
-
-// Check for sign in form submission
-const signInForm = document.querySelector('#sign-in-form');
-if (signInForm) {
-    signInForm.addEventListener('submit', (event) => {
-        isSigningIn = true;
-    });
-}
-
 onAuthStateChanged(auth, (user) => {
     const loadingElement = document.getElementById('loading');
     const mainContentElement = document.getElementById('main-content');
 
     if (user) {
-        // User is signed in
         console.log('User is signed in');
 
-        // User accessing sign in page while signed in
-        if (window.location.pathname === '/sign-in.html' && !isSigningIn) {
-            
-            signOut(auth).then(() => {
-                console.log('User signed out successfully');
-                window.location.href = '/sign-in.html'; // After signing out, redirect to the sign-in page
-            }).catch((error) => {
-                console.error('Error signing out:', error);
-            });
-
-        } else {
-    
-            if (loadingElement && mainContentElement) {
-                loadingElement.style.display = 'none';
-                mainContentElement.style.display = 'block';
-            }
+        // Show main content if not signing out
+        if (loadingElement && mainContentElement) {
+            loadingElement.style.display = 'none';
+            mainContentElement.style.display = 'block';
         }
+    
     } else {
-        // No user is signed in
         console.log('No user is signed in');
 
-        // User is trying to access restricted pages
+        // Redirect to sign-in if trying to access restricted pages
         if (window.location.pathname === '/journal.html' || window.location.pathname === '/log.html' || window.location.pathname === '/setting.html') {
             window.location.href = '/sign-in.html';
         } else {
-            
             if (loadingElement && mainContentElement) {
                 loadingElement.style.display = 'none';
                 mainContentElement.style.display = 'block';
             }
         }
     }
-
-    // Reset the signing in bool
-    isSigningIn = false;
 });
 
 export { auth, db };
